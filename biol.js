d3d213061754cdbd777fbdbcaa26e217bb8a5ffe
petronous <48959829+Petronous@users.noreply.github.com>
2021-12-15 18:29:10 +0100

    fix shuffle fonction

--- biol.js
+++ biol.js
@@ -52,7 +52,7 @@ unusedQs = JSON.parse(localStorage.getItem('uqs'));
 console.log(unusedQs);
 
 function shuffle(arr) {
-  for(let i = arr.length — 1; i > 0; i--){
+  for(let i = arr.length - 1; i > 0; i--){
     const j = Math.floor(Math.random() * i);
     const temp = arr[i];
     arr[i] = arr[j];
